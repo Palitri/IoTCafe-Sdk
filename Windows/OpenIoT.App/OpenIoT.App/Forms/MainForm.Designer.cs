2bbdea86988eefa63d8cd29bbe7f72c3f3bf88d0
Ivan Donev <i.donev@palitri.com>
2025-08-13 20:02:01 +0300

    Removed cnc test commands

--- Windows/OpenIoT.App/OpenIoT.App/Forms/MainForm.Designer.cs
+++ Windows/OpenIoT.App/OpenIoT.App/Forms/MainForm.Designer.cs
@@ -30,7 +30,11 @@
         {
             components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
-            menuMain = new MenuStrip();
+            notifyIcon = new NotifyIcon(components);
+            contextMenuTray = new ContextMenuStrip(components);
+            openToolStripMenuItem = new ToolStripMenuItem();
+            exitToolStripMenuItem = new ToolStripMenuItem();
+            propertiesListControl = new Palitri.OpenIoT.App.Controls.PropertiesListControl();
             optionsToolStripMenuItem = new ToolStripMenuItem();
             loginToolStripMenuItem = new ToolStripMenuItem();
             projectsToolStripMenuItem = new ToolStripMenuItem();
@@ -39,126 +43,10 @@
             boardInfoToolStripMenuItem = new ToolStripMenuItem();
             presetToolStripMenuItem = new ToolStripMenuItem();
             comToolStripMenuItem = new ToolStripMenuItem();
-            testToolStripMenuItem = new ToolStripMenuItem();
-            toolStripMenuItem2 = new ToolStripMenuItem();
-            toolStripMenuItem3 = new ToolStripMenuItem();
-            toolStripMenuItem4 = new ToolStripMenuItem();
-            toolStripMenuItem5 = new ToolStripMenuItem();
-            notifyIcon = new NotifyIcon(components);
-            contextMenuTray = new ContextMenuStrip(components);
-            openToolStripMenuItem = new ToolStripMenuItem();
-            exitToolStripMenuItem = new ToolStripMenuItem();
-            propertiesListControl = new OpenIoT.App.Controls.PropertiesListControl();
-            toolStripMenuItem6 = new ToolStripMenuItem();
-            menuMain.SuspendLayout();
+            menuMain = new MenuStrip();
             contextMenuTray.SuspendLayout();
+            menuMain.SuspendLayout();
             SuspendLayout();
-            // 
-            // menuMain
-            // 
-            menuMain.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
-            menuMain.ImageScalingSize = new Size(32, 32);
-            menuMain.Items.AddRange(new ToolStripItem[] { optionsToolStripMenuItem, presetToolStripMenuItem, comToolStripMenuItem, testToolStripMenuItem });
-            menuMain.Location = new Point(0, 0);
-            menuMain.Name = "menuMain";
-            menuMain.Size = new Size(800, 45);
-            menuMain.TabIndex = 0;
-            menuMain.Text = "menuMain";
-            // 
-            // optionsToolStripMenuItem
-            // 
-            optionsToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { loginToolStripMenuItem, projectsToolStripMenuItem, presetsToolStripMenuItem1, boardNameToolStripMenuItem, boardInfoToolStripMenuItem });
-            optionsToolStripMenuItem.Image = (Image)resources.GetObject("optionsToolStripMenuItem.Image");
-            optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
-            optionsToolStripMenuItem.Size = new Size(52, 41);
-            // 
-            // loginToolStripMenuItem
-            // 
-            loginToolStripMenuItem.Image = (Image)resources.GetObject("loginToolStripMenuItem.Image");
-            loginToolStripMenuItem.Name = "loginToolStripMenuItem";
-            loginToolStripMenuItem.Size = new Size(296, 46);
-            loginToolStripMenuItem.Text = Resources.Login;
-            loginToolStripMenuItem.Click += loginToolStripMenuItem_Click;
-            // 
-            // projectsToolStripMenuItem
-            // 
-            projectsToolStripMenuItem.Image = (Image)resources.GetObject("projectsToolStripMenuItem.Image");
-            projectsToolStripMenuItem.Name = "projectsToolStripMenuItem";
-            projectsToolStripMenuItem.Size = new Size(296, 46);
-            projectsToolStripMenuItem.Text = Resources.Projects;
-            projectsToolStripMenuItem.Click += projectsToolStripMenuItem_Click;
-            // 
-            // presetsToolStripMenuItem1
-            // 
-            presetsToolStripMenuItem1.Image = (Image)resources.GetObject("presetsToolStripMenuItem1.Image");
-            presetsToolStripMenuItem1.Name = "presetsToolStripMenuItem1";
-            presetsToolStripMenuItem1.Size = new Size(296, 46);
-            presetsToolStripMenuItem1.Text = Resources.Presets;
-            presetsToolStripMenuItem1.Click += presetsToolStripMenuItem1_Click;
-            // 
-            // boardNameToolStripMenuItem
-            // 
-            boardNameToolStripMenuItem.Image = Resources.icon_edit;
-            boardNameToolStripMenuItem.Name = "boardNameToolStripMenuItem";
-            boardNameToolStripMenuItem.Size = new Size(296, 46);
-            boardNameToolStripMenuItem.Text = "Board name";
-            boardNameToolStripMenuItem.Click += boardNameToolStripMenuItem_Click;
-            // 
-            // boardInfoToolStripMenuItem
-            // 
-            boardInfoToolStripMenuItem.Image = (Image)resources.GetObject("boardInfoToolStripMenuItem.Image");
-            boardInfoToolStripMenuItem.Name = "boardInfoToolStripMenuItem";
-            boardInfoToolStripMenuItem.Size = new Size(296, 46);
-            boardInfoToolStripMenuItem.Text = Resources.BoardInfo;
-            boardInfoToolStripMenuItem.Click += boardInfoToolStripMenuItem_Click;
-            // 
-            // presetToolStripMenuItem
-            // 
-            presetToolStripMenuItem.Image = (Image)resources.GetObject("presetToolStripMenuItem.Image");
-            presetToolStripMenuItem.Name = "presetToolStripMenuItem";
-            presetToolStripMenuItem.Size = new Size(52, 41);
-            // 
-            // comToolStripMenuItem
-            // 
-            comToolStripMenuItem.Alignment = ToolStripItemAlignment.Right;
-            comToolStripMenuItem.Image = (Image)resources.GetObject("comToolStripMenuItem.Image");
-            comToolStripMenuItem.Name = "comToolStripMenuItem";
-            comToolStripMenuItem.Size = new Size(52, 41);
-            // 
-            // testToolStripMenuItem
-            // 
-            testToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { toolStripMenuItem2, toolStripMenuItem3, toolStripMenuItem4, toolStripMenuItem5, toolStripMenuItem6 });
-            testToolStripMenuItem.Name = "testToolStripMenuItem";
-            testToolStripMenuItem.Size = new Size(80, 41);
-            testToolStripMenuItem.Text = "test";
-            // 
-            // toolStripMenuItem2
-            // 
-            toolStripMenuItem2.Name = "toolStripMenuItem2";
-            toolStripMenuItem2.Size = new Size(359, 46);
-            toolStripMenuItem2.Text = "1";
-            toolStripMenuItem2.Click += toolStripMenuItem2_Click;
-            // 
-            // toolStripMenuItem3
-            // 
-            toolStripMenuItem3.Name = "toolStripMenuItem3";
-            toolStripMenuItem3.Size = new Size(359, 46);
-            toolStripMenuItem3.Text = "2";
-            toolStripMenuItem3.Click += toolStripMenuItem3_Click;
-            // 
-            // toolStripMenuItem4
-            // 
-            toolStripMenuItem4.Name = "toolStripMenuItem4";
-            toolStripMenuItem4.Size = new Size(359, 46);
-            toolStripMenuItem4.Text = "3";
-            toolStripMenuItem4.Click += toolStripMenuItem4_Click;
-            // 
-            // toolStripMenuItem5
-            // 
-            toolStripMenuItem5.Name = "toolStripMenuItem5";
-            toolStripMenuItem5.Size = new Size(359, 46);
-            toolStripMenuItem5.Text = "4";
-            toolStripMenuItem5.Click += toolStripMenuItem5_Click;
             // 
             // notifyIcon
             // 
@@ -193,18 +81,82 @@
             // propertiesListControl
             // 
             propertiesListControl.Dock = DockStyle.Fill;
-            propertiesListControl.Location = new Point(0, 45);
+            propertiesListControl.Location = new Point(0, 40);
             propertiesListControl.Name = "propertiesListControl";
-            propertiesListControl.Size = new Size(800, 405);
+            propertiesListControl.Size = new Size(800, 410);
             propertiesListControl.TabIndex = 1;
             propertiesListControl.PropertyClicked += propertiesListControl_PropertyClicked;
             // 
-            // toolStripMenuItem6
+            // optionsToolStripMenuItem
             // 
-            toolStripMenuItem6.Name = "toolStripMenuItem6";
-            toolStripMenuItem6.Size = new Size(359, 46);
-            toolStripMenuItem6.Text = "5";
-            toolStripMenuItem6.Click += toolStripMenuItem6_Click;
+            optionsToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { loginToolStripMenuItem, projectsToolStripMenuItem, presetsToolStripMenuItem1, boardNameToolStripMenuItem, boardInfoToolStripMenuItem });
+            optionsToolStripMenuItem.Image = (Image)resources.GetObject("optionsToolStripMenuItem.Image");
+            optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            optionsToolStripMenuItem.Size = new Size(52, 36);
+            // 
+            // loginToolStripMenuItem
+            // 
+            loginToolStripMenuItem.Image = (Image)resources.GetObject("loginToolStripMenuItem.Image");
+            loginToolStripMenuItem.Name = "loginToolStripMenuItem";
+            loginToolStripMenuItem.Size = new Size(359, 46);
+            loginToolStripMenuItem.Text = "Login";
+            loginToolStripMenuItem.Click += loginToolStripMenuItem_Click;
+            // 
+            // projectsToolStripMenuItem
+            // 
+            projectsToolStripMenuItem.Image = (Image)resources.GetObject("projectsToolStripMenuItem.Image");
+            projectsToolStripMenuItem.Name = "projectsToolStripMenuItem";
+            projectsToolStripMenuItem.Size = new Size(359, 46);
+            projectsToolStripMenuItem.Text = "Projects";
+            projectsToolStripMenuItem.Click += projectsToolStripMenuItem_Click;
+            // 
+            // presetsToolStripMenuItem1
+            // 
+            presetsToolStripMenuItem1.Image = (Image)resources.GetObject("presetsToolStripMenuItem1.Image");
+            presetsToolStripMenuItem1.Name = "presetsToolStripMenuItem1";
+            presetsToolStripMenuItem1.Size = new Size(359, 46);
+            presetsToolStripMenuItem1.Text = "Presets";
+            presetsToolStripMenuItem1.Click += presetsToolStripMenuItem1_Click;
+            // 
+            // boardNameToolStripMenuItem
+            // 
+            boardNameToolStripMenuItem.Image = (Image)resources.GetObject("boardNameToolStripMenuItem.Image");
+            boardNameToolStripMenuItem.Name = "boardNameToolStripMenuItem";
+            boardNameToolStripMenuItem.Size = new Size(359, 46);
+            boardNameToolStripMenuItem.Text = "Board name";
+            boardNameToolStripMenuItem.Click += boardNameToolStripMenuItem_Click;
+            // 
+            // boardInfoToolStripMenuItem
+            // 
+            boardInfoToolStripMenuItem.Image = (Image)resources.GetObject("boardInfoToolStripMenuItem.Image");
+            boardInfoToolStripMenuItem.Name = "boardInfoToolStripMenuItem";
+            boardInfoToolStripMenuItem.Size = new Size(359, 46);
+            boardInfoToolStripMenuItem.Text = "Board info";
+            boardInfoToolStripMenuItem.Click += boardInfoToolStripMenuItem_Click;
+            // 
+            // presetToolStripMenuItem
+            // 
+            presetToolStripMenuItem.Image = (Image)resources.GetObject("presetToolStripMenuItem.Image");
+            presetToolStripMenuItem.Name = "presetToolStripMenuItem";
+            presetToolStripMenuItem.Size = new Size(52, 36);
+            // 
+            // comToolStripMenuItem
+            // 
+            comToolStripMenuItem.Alignment = ToolStripItemAlignment.Right;
+            comToolStripMenuItem.Image = (Image)resources.GetObject("comToolStripMenuItem.Image");
+            comToolStripMenuItem.Name = "comToolStripMenuItem";
+            comToolStripMenuItem.Size = new Size(52, 36);
+            // 
+            // menuMain
+            // 
+            menuMain.Font = new Font("Segoe UI", 10F);
+            menuMain.ImageScalingSize = new Size(32, 32);
+            menuMain.Items.AddRange(new ToolStripItem[] { optionsToolStripMenuItem, presetToolStripMenuItem, comToolStripMenuItem });
+            menuMain.Location = new Point(0, 0);
+            menuMain.Name = "menuMain";
+            menuMain.Size = new Size(800, 40);
+            menuMain.TabIndex = 0;
+            menuMain.Text = "menuMain";
             // 
             // MainForm
             // 
@@ -222,34 +174,27 @@
             FormClosing += MainForm_FormClosing;
             FormClosed += Form1_FormClosed;
             SizeChanged += MainForm_SizeChanged;
+            contextMenuTray.ResumeLayout(false);
             menuMain.ResumeLayout(false);
             menuMain.PerformLayout();
-            contextMenuTray.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
-
-        private MenuStrip menuMain;
-        private ToolStripMenuItem optionsToolStripMenuItem;
-        private ToolStripMenuItem boardInfoToolStripMenuItem;
-        private ToolStripMenuItem loginToolStripMenuItem;
-        private ToolStripMenuItem projectsToolStripMenuItem;
-        private ToolStripMenuItem presetsToolStripMenuItem1;
-        private ToolStripMenuItem presetToolStripMenuItem;
-        private ToolStripMenuItem comToolStripMenuItem;
         private NotifyIcon notifyIcon;
         private ContextMenuStrip contextMenuTray;
         private ToolStripMenuItem exitToolStripMenuItem;
         private ToolStripMenuItem openToolStripMenuItem;
-        private ToolStripMenuItem boardNameToolStripMenuItem;   
         private Controls.PropertiesListControl propertiesListControl;
-        private ToolStripMenuItem testToolStripMenuItem;
-        private ToolStripMenuItem toolStripMenuItem2;
-        private ToolStripMenuItem toolStripMenuItem3;
-        private ToolStripMenuItem toolStripMenuItem4;
-        private ToolStripMenuItem toolStripMenuItem5;
-        private ToolStripMenuItem toolStripMenuItem6;
+        private ToolStripMenuItem optionsToolStripMenuItem;
+        private ToolStripMenuItem loginToolStripMenuItem;
+        private ToolStripMenuItem projectsToolStripMenuItem;
+        private ToolStripMenuItem presetsToolStripMenuItem1;
+        private ToolStripMenuItem boardNameToolStripMenuItem;
+        private ToolStripMenuItem boardInfoToolStripMenuItem;
+        private ToolStripMenuItem presetToolStripMenuItem;
+        private ToolStripMenuItem comToolStripMenuItem;
+        private MenuStrip menuMain;
     }
 }
